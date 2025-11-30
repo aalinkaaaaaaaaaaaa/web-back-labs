51024b2f28b29de43ba67bc45667e8bd907d8e7d
Алина Геворкян <aalinkaaaaaaaaaaa@vk.com>
2025-11-30 18:45:09 +0700

    Самостоятельные задания

--- lab7.py
+++ lab7.py
@@ -90,6 +90,11 @@ def put_film(id):
     if not film_data:
         return jsonify({"error": "Не предоставлены данные для обновления"}), 400
     
+    title = film_data.get('title', '')
+    title_ru = film_data.get('title_ru', '')
+    if not title.strip() and title_ru.strip():
+        film_data['title'] = title_ru
+
     if 'description' not in film_data or not film_data['description'].strip():
         return jsonify({'description': 'Заполните описание'}), 400
     
@@ -104,6 +109,11 @@ def add_film():
     if not film_data:
         return jsonify({"error": "Не предоставлены данные фильма"}), 400
     
+    title = film_data.get('title', '')
+    title_ru = film_data.get('title_ru', '')
+    if not title.strip() and title_ru.strip():
+        film_data['title'] = title_ru
+        
     if 'description' not in film_data or not film_data['description'].strip():
         return jsonify({'description': 'Заполните описание'}), 400
     
