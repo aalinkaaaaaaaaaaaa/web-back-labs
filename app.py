371fdb5b7e0e0a77d5d5016e130dd4e13e69394e
Алина Геворкян <aalinkaaaaaaaaaaa@vk.com>
2025-12-10 23:34:45 +0700

    БД

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, url_for, request
 import os
 import datetime
 from dotenv import load_dotenv
-from models import db, Office
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
@@ -19,11 +18,6 @@ load_dotenv()
 app.secret_key = os.getenv('SECRET_KEY', 'default-secret-key')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'sqlite')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///offices.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db.init_app(app)
-
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
@@ -33,24 +27,6 @@ app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
 access_log = []
-
-
-with app.app_context():
-    db.create_all()
-
-    if Office.query.count() == 0:
-        offices_data = []
-        for i in range(1, 11):
-            offices_data.append(Office(
-                number=i,
-                tenant='',
-                price=900 + i % 3 * 100
-            ))
-        
-        db.session.add_all(offices_data)
-        db.session.commit()
-        print("База данных инициализирована с офисами")
-
 
 @app.errorhandler(404)
 def not_found(err):
